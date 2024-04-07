911ca54bacc1ad3388346c979db3362bddd9115c
Chia Yu Yang <a8829037@gmail.com>
2024-04-07 13:24:12 +0800

    test

--- autoload.js
+++ autoload.js
@@ -36,7 +36,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			apiPath: "https://cloud.iddle.dev/live_2d/api/",
-			// cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
